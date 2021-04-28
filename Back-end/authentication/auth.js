2077f10effbf8c9695ccedbaaf84aee60adf796d
DIAB ELMEHDI <diab.elmehd@gmail.com>
2021-04-28 17:33:28 +0200

    login

--- Back-end/authentication/auth.js
+++ Back-end/authentication/auth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 const handle = require("../handlers");
 router.post("/register", handle.register);
-router.post("/login");
+router.post("/login", handle.login);
 module.exports = router;
--- Back-end/handlers/auth.js
+++ Back-end/handlers/auth.js
@@ -2,15 +2,27 @@ const db = require("../models");
 
 exports.register = async (req, res, next) => {
   try {
-    const user = await db.user.create(req.body);
-
-    res.json(user);
+    const user = await db.User.create(req.body);
+    const { id, username } = user;
+    res.json(id, username);
   } catch (err) {
     next(err);
   }
 };
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   try {
+    const user = await db.User.findOne({ username: req.body.username });
+    const { id, username } = user;
+    const valid = await user.comparePassword(req.body.password);
+
+    if (valid) {
+      res.json({
+        id,
+        username,
+      });
+    } else {
+      throw new Error("invalid Username/Password");
+    }
   } catch (err) {
     next(err);
   }

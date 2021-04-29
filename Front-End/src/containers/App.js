60d4bfc3f9f1b1a38efefee53158e430792a5e62
DIAB ELMEHDI <diab.elmehd@gmail.com>
2021-04-29 02:15:13 +0200

    front end handlling

--- Front-End/src/containers/App.js
+++ Front-End/src/containers/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Provider } from "react-redux";
-import api from "../services/api";
+import { BrowserRouter as Router } from "react-router-dom";
+import decode from "jwt-decode";
+
 import { store } from "../store";
+import { setToken, setCurrentUser, addError } from "../store/actions";
+
+import NavBar from "./NavBar";
+import RouteViews from "./RouteViews";
+
+if (localStorage.jwtToken) {
+  setToken(localStorage.jwtToken);
+  try {
+    store.dispatch(setCurrentUser(decode(localStorage.jwtToken)));
+  } catch (err) {
+    store.dispatch(setCurrentUser({}));
+    store.dispatch(addError(err));
+  }
+}
+
 const App = () => (
-  <Provider>
-    <dive>App works</dive>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <NavBar />
+        <RouteViews />
+      </Fragment>
+    </Router>
   </Provider>
 );
+
 export default App;
